d7f85df36c243031f016c9e9e71b6e72cbac0589
Furkan AKTAŞ <95107035+furkannaktass@users.noreply.github.com>
2022-08-05 23:50:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnBüyükDiziSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write("sayi giriniz:");
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(sayilar);
+            foreach (var i in sayilar)
+            {
+                Console.WriteLine("{0}",i);
+            }
+
+            Console.WriteLine("{0} en büyük toplam", sayilar[8] + sayilar[9]);
+            Console.ReadLine();
+        }
+    }
+}
